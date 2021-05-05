2d34dfa4e2294fcdb0eb9fc113d006ab5c85f5d2
Bigon4ik <bigon444ik@gmail.com>
2021-05-05 16:56:26 +0300

    refactoring Select

--- src/component/Select/Select.tsx
+++ src/component/Select/Select.tsx
@@ -39,10 +39,18 @@ export function Select(props: SelectPropsType) {
                         : props.items[i - 1]
                     if (pretendentElement) {
                         props.onChange(pretendentElement.value)
-                        break;
+                        return;
                     }
                 }
             }
+            if(!selectedItems){
+                props.onChange(props.items[0].value)
+            }
+
+        }
+
+        if(e.key === "Enter" || e.key==="Escape"){
+            setActive(false)
         }
     }
     return (
