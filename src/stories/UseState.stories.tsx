a5ce20b13744d348fcffa8e41a464edd65232332
Bigon4ik <bigon444ik@gmail.com>
2021-05-18 23:05:27 +0300

    add useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,28 @@
+import React, {useMemo, useState} from 'react';
+
+export default {
+    title: 'UseState / UseState stories',
+
+}
+
+function generateData() {
+    return 1
+}
+
+export const Example1 = () => {
+
+    // const initValue = useMemo(generateData, [])
+
+    const [counter, setCounter] = useState(generateData)
+
+    const changer = (state: number) => {
+        return state + 1
+
+    }
+    return <>
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </>
+
+}
+
